b001cfb99f0a24461acddb65017a47161a8b7f0f
quanglong292 <quang.long292@gmail.com>
2024-12-10 13:56:52 +0700

    wip

--- src/app/(components)/Footer.jsx
+++ src/app/(components)/Footer.jsx
@@ -6,11 +6,16 @@ import { ROUTES } from "@/shared/routes";
 import React from "react";
 import { twMerge } from "tailwind-merge";
 
-export const SocialIcon = ({ children }) => {
+export const SocialIcon = ({ children, link }) => {
   return (
-    <div className="transition-all hover:bg-primary w-10 h-10 flex justify-center items-center bg-primary-900 text-white rounded-md">
+    <Link
+      href={link}
+      target="_blank"
+      prefetch={false}
+      className="transition-all hover:bg-primary w-10 h-10 flex justify-center items-center bg-primary-900 text-white rounded-md"
+    >
       {children}
-    </div>
+    </Link>
   );
 };
 
@@ -91,7 +96,7 @@ const Footer = () => {
             chatbot trained on your website content.
           </Text>
           <div className="flex gap-4">
-            <SocialIcon>
+            <SocialIcon link="https://www.facebook.com/p/QKIT-Software-100076370654780/?locale=vi_VN">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -121,7 +126,7 @@ const Footer = () => {
                 <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
               </svg>
             </SocialIcon>
-            <SocialIcon>
+            <SocialIcon link="https://vn.linkedin.com/company/qkit">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -137,11 +142,18 @@ const Footer = () => {
         <div className="flex flex-col gap-4 w-full md:w-1/6">
           <Header2 className="md:text-2xl">Product</Header2>
           <div className="flex flex-col gap-4">
-            {[ROUTES[0]].map((item) => (
-              <LinkToPage key={item.route} href={item.route}>
-                {item.text}
-              </LinkToPage>
-            ))}
+            {["Features", "Integrations", "AI Chatbot", "AI Service"]
+              .map((text) => {
+                const item = ROUTES[0];
+                return { ...item, text };
+              })
+              .map((item) => {
+                return (
+                  <LinkToPage key={item.route} href={item.route}>
+                    {item.text}
+                  </LinkToPage>
+                );
+              })}
             {/* <LinkToPage href="#">Chatbots</LinkToPage>
             <LinkToPage href="#">Chatbots</LinkToPage> */}
           </div>
